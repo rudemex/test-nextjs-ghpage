3d51cd4633af9d7d1f0617ff5b4d98ccb14afcd2
rudemex <mex.skarlatina@gmail.com>
2021-02-07 20:17:07 +0000

    Deploying to gh-pages from  @ dc5b264ccab19dc9041229aefcb52fe0c0f01aeb 🚀

---
+++ _next/static/1z8_wnUSUS1DP8lfGHifj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-f0150a67c9e7e6793ee5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-79c0747ed0241d18db46.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
