65329a2d5bddf961c8a793839cdab799a0e1260f
Maximiliano Delgado <mdelgado@baufest.com>
2021-02-07 17:21:35 -0300

    edit workflow

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
     // Use the CDN in production and localhost for development.
-    assetPrefix: isProd ? 'https://cdn.statically.io/gh/rudemex/rudemex.github.io/test-nextjs-ghpage/' : '',
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/rudemex/test-nextjs-ghpage/gh-pages/' : '',
 }
