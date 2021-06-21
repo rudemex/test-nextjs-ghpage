5aadd19d536edbd6e7c6f8882fa5867f52996a56
Maximiliano Delgado <mdelgado@baufest.com>
2021-06-21 13:07:08 -0300

    chore: change text in home

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js 11!</a>
         </h1>
 
         <p className={styles.description}>
